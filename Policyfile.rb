7b718443abb55ca0666507c28726365745ac8d82
Komal Kadam <kadamkomal2008@gmail.com>
2020-01-24 18:33:56 +0530

    Update Policyfile.rb

--- Policyfile.rb
+++ Policyfile.rb
@@ -4,13 +4,13 @@
 # https://docs.chef.io/policyfile.html
 
 # A name that describes what the system you're building with Chef does.
-name 'chef-rean-java'
+name 'chef-nginx'
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'chef-rean-java::default'
+run_list 'chef-nginx::default'
 
 # Specify a custom source for a single cookbook:
-cookbook 'chef-rean-java', path: '.'
+cookbook 'chef-nginx', path: '.'
